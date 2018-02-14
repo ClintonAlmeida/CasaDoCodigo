75f8c2b1448f697d094db0f33317b89c58e50d20
clinton.almeida <clinton.almeida@spnb02ti.GiraffasSP.com>
2018-02-13 22:26:52 -0200

    PRIMEIRO COMMIT AULA 6 FINAL RODANDO

---
+++ src/main/java/br/com/casadocodigo/loja/daos/ProdutoDAO.java
@@ -0,0 +1,37 @@
+package br.com.casadocodigo.loja.daos;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.casadocodigo.loja.models.Produto;
+
+@Repository
+@Transactional
+public class ProdutoDAO {
+
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	public void gravar(Produto produto) {
+
+		entityManager.persist(produto);
+	}
+
+	public EntityManager getEntityManager() {
+		return entityManager;
+	}
+
+	public void setEntityManager(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
+
+	public List<Produto> listar() {
+
+		return entityManager.createQuery("select p from Produto p", Produto.class).getResultList();
+	}
+}

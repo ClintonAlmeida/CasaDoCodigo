b2ad9ed2d6b5d39b5f1f768541d6eba4a58e7819
Clinton <clinton.anti@gmail.com>
2018-02-25 19:57:49 -0300

    Capitulo 10
    Funcionando

--- src/main/java/br/com/casadocodigo/loja/daos/ProdutoDAO.java
+++ src/main/java/br/com/casadocodigo/loja/daos/ProdutoDAO.java
@@ -34,4 +34,11 @@ public class ProdutoDAO {
 
 		return entityManager.createQuery("select p from Produto p", Produto.class).getResultList();
 	}
+
+	public Produto find(Integer id) {
+		// TODO Auto-generated method stub
+		return entityManager.createQuery("select distinct(p) from Produto p join fetch p.precos precos where p.id = :id ", Produto.class).setParameter("id", id).getSingleResult();
+		
+		
+	}
 }
